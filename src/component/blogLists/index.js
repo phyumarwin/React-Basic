6aea61329f4bbbaa706f11204959a64555f58997
phyumarwin <phyumarwin99309@gmail.com>
2025-04-03 14:47:15 +0630

    first commit

---
+++ src/component/blogLists/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+export default function BlogLists({blogs}) {
+  return (
+    <div className='blogLists'>
+      {
+        blogs.map(blog => (
+          <div className='single_blog' key={blog.id}>{blog.title}</div>
+        ))
+      }    
+    </div>
+  )
+}
---
+++ src/component/nav/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className='container'>
+        <h1>Logo</h1>
+        <ul>
+          <li>Home</li>
+          <li>Blogs</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
